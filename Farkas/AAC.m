fa08220468f12dbe0b0c386797c63e4611895476
farkaslory <65665990+farkaslory@users.noreply.github.com>
2026-07-30 17:41:11 +0200

    Egy LDPC kódolóra az aszinkron moduláció

---
+++ Farkas/AAC.m
@@ -0,0 +1,63 @@
+clear;
+H = dvbs2ldpc(1/2)
+cfgLDPCEnc = ldpcEncoderConfig(H)
+numframes = 1;
+cfgLDPCDec = ldpcDecoderConfig(H)
+n = cfgLDPCEnc.NumInformationBits;
+BlockLengthHalf = cfgLDPCDec.BlockLength/2
+K = 20; % Number of codewords to process
+LdpcErr_2=[]
+place=zeros(1,20);
+for snr=1:0.3:3
+    error=zeros(1,1000);
+    for k=1:1000
+        for j=1:K
+            data{j} = randi([0 1],n,numframes,'int8');
+        end
+        for j=1:K
+            codedData{j} = Code(data{j},cfgLDPCEnc);
+            cD1{j}=reshape(codedData{j},[2 BlockLengthHalf])';
+        end
+        input=pskmod(cD1{1}(:,1),2,0);
+        for j=1:K-1
+            input1 = bi2de([cD1{j}(:,2),cD1{j+1}(:,1)],"left-msb");
+            input = [input;pskmod(input1,4,pi/4)];
+        end
+        input = [input;pskmod(cD1{K}(:,2),2,0)];
+        output = awgn(input,snr);
+        for j=1:K-1
+            LLR1Half{j} = pskdemod(output(BlockLengthHalf*(j-1)+1:BlockLengthHalf*j),2,0,OutputType="llr");
+            LLR2Half = reshape(pskdemod(output(BlockLengthHalf*j+1:BlockLengthHalf*(j+1)),4,pi/4,OutputType="llr"),[2 BlockLengthHalf])';
+            LLR{j} = [LLR1Half{j},LLR2Half(:,1)];
+            LlrActual=LLR{j}';
+            decodedData{j} = Decode(LlrActual(:),cfgLDPCDec);
+            EstimatedCodeword = Code(decodedData{j},cfgLDPCEnc);
+            EstimatedCodeword = reshape(EstimatedCodeword,[2 BlockLengthHalf])';
+            SecondPartECWinFourier = pskmod(EstimatedCodeword(:,2),2,pi/2);
+            output(BlockLengthHalf*j+1:BlockLengthHalf*(j+1))=output(BlockLengthHalf*j+1:BlockLengthHalf*(j+1))-1/sqrt(2)*SecondPartECWinFourier;    
+        end
+        LLR{K} = reshape(pskdemod(output(BlockLengthHalf*(20-1)+1:BlockLengthHalf*(20+1)),2,0,OutputType="llr"),[BlockLengthHalf 2]);
+        LlrActual=LLR{K}';
+        decodedData{K} = Decode(LlrActual(:),cfgLDPCDec);
+        for j=1:K
+            ErrCount(j)=biterr(data{j},decodedData{j});
+        end
+        error(k)=sum(ErrCount)/K;
+        if ErrCount ~= 0
+            [m,j]=max(ErrCount);
+            place(j)=place(j)+1;
+        end
+    end
+    LdpcErr_2(end+1)=mean(error)/n;
+    snr
+end
+plot(1:0.3:3,LdpcErr_2)
+bar(place)
+
+function output=Code(InfBits,Params)
+    output = ldpcEncode(InfBits,Params);
+end
+function EstimatedInfBits=Decode(LogLikelihoodRation,Params)
+    maxnumiter = 10;
+    EstimatedInfBits = ldpcDecode(LogLikelihoodRation,Params,maxnumiter);
+end
